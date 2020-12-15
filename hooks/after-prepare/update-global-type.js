93b899f09222ddefded6c140c41c8ba4ecf9086e
Adam Atri <adam@atricoware.com>
2020-12-15 10:48:10 -0500

    fixes global type for {N} compilation

---
+++ hooks/after-prepare/update-global-type.js
@@ -0,0 +1,9 @@
+const shell = require('shelljs');
+const { join } = require('path');
+
+module.exports = function replaceGlobalType($logger, $projectData, hookArgs) {
+  shell.cd(join($projectData.projectDir, 'node_modules', '@types', 'node'));
+  const files = shell.find('globals.global.d.ts');
+  if (files.length)
+    shell.sed('-i', "NodeJS.Global ", "NodeJS.Global & typeof globalThis", ...files);
+}
